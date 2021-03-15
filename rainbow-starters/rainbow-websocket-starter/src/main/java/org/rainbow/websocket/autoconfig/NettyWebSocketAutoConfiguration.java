6095c6d0a08fdd2ffd7856ed0127a2aed276eef7
qq1132434165 <1132434165@qq.com>
2021-03-15 11:49:49 +0800

    ✔ rainbow-websocket-starter补充内容

---
+++ rainbow-starters/rainbow-websocket-starter/src/main/java/org/rainbow/websocket/autoconfig/NettyWebSocketAutoConfiguration.java
@@ -0,0 +1,11 @@
+package org.rainbow.websocket.autoconfig;
+
+import org.rainbow.websocket.annotation.EnableWebSocket;
+
+/**
+ * @author K
+ * @date 2021/3/15  11:44
+ */
+@EnableWebSocket
+public class NettyWebSocketAutoConfiguration {
+}
